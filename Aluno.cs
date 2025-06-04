69ad5c8f38c7d4b63ca77f8399796d04c691dc9d
PedroHDantas <pedrohdantaspc@gmail.com>
2025-06-04 17:37:26 -0300

    feat: implementado a classe aluno e suas propriedades

---
+++ Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace Modulo01EX03
+{
+    internal class Aluno
+    {
+        public string nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+    }
+}
