e4a2be13fb50261e5f269bbc4649b85be07ecc62
PedroHDantas <pedrohdantaspc@gmail.com>
2025-06-04 18:45:25 -0300

    feat: implementado métodos para calcular nota, aprovado/reprovado e nota restante

--- Aluno.cs
+++ Aluno.cs
@@ -17,5 +17,27 @@ namespace Modulo01EX03
             N2 = n2;
             N3 = n3;
         }
+
+        public double  NotaFinal()
+        {
+            return N1 + N2 + N3;
+           
+        }
+        public bool VerificarNota()
+        {
+            if (NotaFinal() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double NotaParaPassar()
+        {
+            return 60.0 - NotaFinal();
+        }
     }
 }
